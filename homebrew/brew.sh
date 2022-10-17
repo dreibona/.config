90916b104428247a79fdff38cfc763aca1af2200
Dreibona <59962148+dreibona@users.noreply.github.com>
2022-10-17 17:02:35 +0200

    add brew install

---
+++ homebrew/brew.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#####################################################################
+# chmod +x ~/.config/homebrew/brew.sh && ~/.config/homebrew brew.sh #
+#####################################################################
+
+########################################
+# Check for install, if not install it #
+########################################
+
+if test ! "$(which brew)"; then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#####################
+# Echo brew version #
+#####################
+
+echo ""
+echo "Brew version brew"
+brew --version
+
+#########################
+# Update before install #
+#########################
+
+echo ""
+brew update
+
+#########################################################################
+# Run Brew bundle, which will install everything in the file "Brewfile" #
+#########################################################################
+
+brew bundle 
+
+##########################
+# List of installed apps #
+##########################
+
+echo ""
+echo "Brew List"
+brew list
