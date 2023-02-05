eb49254dc4f2d90dcf1d3cc0c603d1aed8f015e2
Dreibona <59962148+dreibona@users.noreply.github.com>
2023-02-05 10:08:27 -0300

    update backup folder

--- scripts/data.sh
+++ scripts/data.sh
@@ -8,5 +8,5 @@ normal=$(tput sgr0)
 echo ""
 echo "${bold}backing up home to data${normal}"
 sudo rsync -varE --delete-delay --progress --include='*/' \
---files-from=$HOME/.config/settings/backup $HOME /Volumes/data/2022_backup
+--files-from=$HOME/.config/settings/backup $HOME /Volumes/data/2023_backup
 echo ""
